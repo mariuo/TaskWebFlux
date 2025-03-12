345dad6814be1fe85b61ca75de42c847b2b37f76
Mario Camelo <mariuo@gmail.com>
2025-03-12 18:51:40 -0400

    feat: Project created

---
+++ src/test/java/com/camel/TaskWebFlux/TaskWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.TaskWebFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
